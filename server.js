31f97dd09812e7aee692e0e256f02d83db0f5087
JakeMackBrown <JakeMackBrown@gmail.com>
2024-11-18 09:19:16 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
