bbfa2638def984017aa6757c49c922892833ed9d
SourGummyes <dewet.co@gmail.com>
2024-11-18 14:24:15 -0500

    server.js

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+app.use(cors())
+
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+  });
